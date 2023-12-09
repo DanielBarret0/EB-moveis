c159fa3881d68be5582b98824abe2838552a56de
Daniel Barreto <99921965+DanielBarret0@users.noreply.github.com>
2023-12-09 00:12:55 -0300

    atualização do index e sobre page

---
+++ assets/js/botao-topo.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var btnScrollToTop = document.getElementById("btnScrollToTop");
+
+    // Mostrar ou ocultar o botão dependendo do scroll da página
+    window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            btnScrollToTop.style.display = "block";
+        } else {
+            btnScrollToTop.style.display = "none";
+        }
+    });
+
+    // Rolagem suave ao topo da página
+    btnScrollToTop.addEventListener("click", function() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+});
